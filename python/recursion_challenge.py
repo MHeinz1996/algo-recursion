bec7aea32eea5e24362afd8176a5845b31e51a54
MHeinz1996 <michael.heinzinger@gmail.com>
2022-06-09 15:32:44 -0500

    Completed all recursive algos except for palindrome so far

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,11 +1,40 @@
 def factorial(x):
-	pass
+	if x == 1:
+		return 	1
+	else:
+		return x * factorial(x-1)
 
 def palindrome(string):
 	pass
 
 def bottles(num):
-	pass
+	if num == 2:
+		return f"{num} bottles of beer on the wall, {num} bottles of beer.\nTake one down and pass it around, {num-1} more bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+	else:
+		return f"{num} bottles of beer on the wall, {num} bottles of beer.\nTake one down and pass it around, {num-1} more bottles of beer on the wall." + f"\n{bottles(num-1)}"
 
 def roman_num(num):
-	pass
+	roman_numeral_to_arabic_map = {
+		'M': 1000,
+		'CM': 900,
+		'D': 500,
+		'CD': 400,
+		'C': 100,
+		'XC': 90,
+		'L': 50,
+		'XL': 40,
+		'X': 10,
+		'IX': 9,
+		'V': 5,
+		'IV': 4,
+		'I': 1,
+	}
+	
+	if num == 0:
+		return ''
+	else:
+		for key in roman_numeral_to_arabic_map:
+			if((num/roman_numeral_to_arabic_map[key]) >= 1):
+				return f"{key}" + f"{roman_num(num - roman_numeral_to_arabic_map[key])}"
+
+print(roman_num(29))
